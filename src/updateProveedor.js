dffcd04feec3bf73c2c991b3a702ef1602038441
Juan Pablo Donalisio <jpdona@hotmail.com>
2023-11-03 21:10:04 -0300

    first commit

---
+++ src/updateProveedor.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports.updateProveedores = async (event) => {
+
+  
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+};

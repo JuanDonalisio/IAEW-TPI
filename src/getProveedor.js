315f18ea340b78460c7130582b36fe6ca9ad99db
Juan Pablo Donalisio <jpdona@hotmail.com>
2023-11-04 14:13:42 -0300

    main

--- src/getProveedor.js
+++ src/getProveedor.js
@@ -3,7 +3,13 @@ const { getProveedorById } = require('../helpers/dynamodbHelper');
 
 module.exports.handler = async (event) => {
   try {
-    await getProveedorById();
+    const id = event.pathParameters.proveedor_id;
+    if (!id) {
+      let allProveedores = await getProveedorById();
+      return { statusCode: 200, body: JSON.stringify(allProveedores)};
+    }
+
+    let proveedor = await getProveedorById(id);
     return { statusCode: 200, body: JSON.stringify({message: 'Go Serverless v1.0! Your function executed successfully!'})};
   } catch (error) {
     console.log(error)
